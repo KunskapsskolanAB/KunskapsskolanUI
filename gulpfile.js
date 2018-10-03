4f60ad1c1c95eaecd2be59e16c4dc9343e532fe6
Carl Holmberg <info@talgdank.se>
2018-10-03 20:11:22 +0200

    Updated gulpfile to actually compress/minify and use same output-directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,60 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var sassLint    = require('gulp-sass-lint');
+let gulp        = require('gulp');
+let sass        = require('gulp-sass');
+let sassLint    = require('gulp-sass-lint');
+let cleanCSS    = require('gulp-clean-css');
+let rename      = require('gulp-rename');
 
 /**
  * Compile files
  */
-gulp.task('normal', function () {
-    return gulp.src('stylesheets/kunskapsskolanUI.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: sass.logError
-        }))
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError())
-        .pipe(gulp.dest('css'))
+ 
+gulp.task('normal', () => {
+  return gulp.src('stylesheets/kunskapsskolanUI.scss')
+    .pipe(sass({
+      includePaths: ['scss'],
+      onError: sass.logError
+   }))
+   .pipe(sassLint())
+   .pipe(sassLint.format())
+   .pipe(sassLint.failOnError())
+   .pipe(gulp.dest('css'))
 });
 
-gulp.task('dev', function () {
-    return gulp.src('stylesheets/dev.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: sass.logError
-        }))
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError())
-        .pipe(gulp.dest('dev_css'))
+
+gulp.task('dev', () => {
+  return gulp.src('stylesheets/dev.scss')
+    .pipe(sass({
+      includePaths: ['scss'],
+      onError: sass.logError
+   }))
+   .pipe(sassLint())
+   .pipe(sassLint.format())
+   .pipe(sassLint.failOnError())
+   .pipe(gulp.dest('css'))
 });
 
-gulp.task('test', function () {
-    return gulp.src('stylesheets/dev_test.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: sass.logError
-        }))
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError())
-        .pipe(gulp.dest('dev_css'))
+gulp.task('test', () => {
+  return gulp.src('stylesheets/dev_test.scss')
+    .pipe(sass({
+      includePaths: ['scss'],
+      onError: sass.logError
+   }))
+   .pipe(sassLint())
+   .pipe(sassLint.format())
+   .pipe(sassLint.failOnError())
+   .pipe(gulp.dest('css'))
 });
 
-gulp.task('compress', function(){
-    return gulp.src('stylesheets/kunskapsskolanUI.scss')
-        .pipe(sass({ 
-            outputStyle: 'compressed',
-            onError: sass.logError 
-        }))
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError())
-        .pipe(gulp.dest('css'))
+gulp.task('compress', () => {
+  return gulp.src('stylesheets/kunskapsskolanUI.scss')
+    .pipe(rename('kunskapsskolanUI.min.scss'))
+    .pipe(sass({ 
+      outputStyle: 'compressed',
+      onError: sass.logError 
+   }))
+   .pipe(sassLint())
+   .pipe(sassLint.format())
+   .pipe(sassLint.failOnError())
+   .pipe(cleanCSS())
+   .pipe(gulp.dest('css'))
 });
